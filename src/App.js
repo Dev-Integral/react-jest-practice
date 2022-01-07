9aa94d2fc87fb95811089275affb9d444e9f4407
Ayoola Taiwo <tayoola13@yahoo.com>
2022-01-07 22:59:28 +0100

    More test cases added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 export const Add = (x, y) => x + y ;
 
 export const Total = (shipping, subTotal) => Add(shipping, subTotal);
+
+export const numberWithComma = function(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+export const functions = {
+    add: (x,y ) => x + y,
+    isNull: ()=> null,
+    checkValue: (x) => x,
+    createUser: ()=> {return {firstName: 'Ayoola', lastName: 'Taiwo'}}
+}
+
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,66 @@
-import {Add, Total} from './App'
+import {Total, numberWithComma, functions} from './App'
 
 // This is a unit test;
 test('Addition of two  numbers', ()=>{
-    const value = Add(2, 3);
+    const value = functions.add(2, 3);
     expect(value).toBe(5);
 });
-// This is an Integration test;
+// This is an Integration test because the Total fxn is dependent on the output of the Add fxn;
 test('Total', ()=> {
     expect(Total(5, 7)).toBe(12);
+});
+
+// This should convert numbers to include commas
+test('number with comma', ()=>{
+    let expNum = numberWithComma(50000);
+    let expNum2 = numberWithComma(50000.23);
+    expect(expNum).toBe('50,000');
+    expect(expNum2).toBe('50,000.23');
+});
+
+// CHECKS FOR TRUTHY AND FALSY VALUES; 0 null undefined
+
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test('Should be null', ()=>{
+    expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test('Should be falsy', ()=>{
+    expect(functions.checkValue(0)).toBeFalsy();
+    expect(functions.checkValue(undefined)).toBeFalsy();
+    expect(functions.checkValue(null)).toBeFalsy();
+    expect(functions.checkValue(false)).toBeFalsy();
+});
+
+// toEqual
+test('User should be Ayoola Taiwo object', ()=>{
+    expect(functions.createUser()).toEqual({
+        firstName: 'Ayoola',
+        lastName: 'Taiwo'
+    });
+});
+
+// Less than and greater than
+test('Should be under 1600', ()=>{
+    const load1 = 800;
+    const load2 = 500;
+    expect(load1+load2).toBeLessThan(1600);
+}) 
+
+// Regex
+test('There is no I in team', ()=>{
+    expect('team').not.toMatch(/I/i);
 })
+
+// Arrays
+test('Admin should be in usernames', () => {
+    const usernames = ['Taiwo', 'Kehinde', 'Admin'];
+    expect(usernames).toContain('Admin');
+});
